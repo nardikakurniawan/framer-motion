c5e8fd997ce1eaba0d46bb0c784d2ef540aab550
Nardika Kurniawan <nardika.kurniawan@lumoshive.com>
2022-11-09 11:44:03 +0700

    Done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import Navbar from "./../components/Navbar";
+import { AnimatePresence } from "framer-motion";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
     <div className="py-8 max-w-6xl mx-auto font-lato font-bold">
       <Navbar />
-      <Component {...pageProps} />
+      <AnimatePresence initial={false}>
+        <Component key={router.pathname} {...pageProps} />
+      </AnimatePresence>
     </div>
   );
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,27 +1,54 @@
 import { motion } from "framer-motion";
-
+import { container, item } from "./../../animation";
 export default function Contact() {
   return (
-    <main>
-      <div className="my-96 p-1">
-        <h1 className="text-6xl text-center lg:text-right lg:text-9xl">
+    <motion.main
+      initial={{ y: "100%" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 0.75, ease: "easeInOut" }}
+      exit={{ opacity: 1 }}
+      className="text-gray-900 absolute top-0 left-0 w-full bg-red-400 lg:px-48 px-16"
+    >
+      <div className="my-96 p-1 overflow-hidden">
+        <motion.h1
+          animate={{ y: 0 }}
+          initial={{ y: "100%" }}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          className="text-6xl text-center lg:text-right lg:text-9xl"
+        >
           Let's Talk
-        </h1>
+        </motion.h1>
       </div>
 
-      <div className="flex gap-4">
+      <div className="flex gap-40">
         <div className="lg:text-2xl text-base">
           <h4>Find me:</h4>
         </div>
-        <div className="lg:text-6xl text-2xl underline">
-          <ul>
-            <li className="pb-2">Twitter</li>
-            <li className="pb-2">Instagram</li>
-            <li className="pb-2">LinkedIn</li>
-            <li className="pb-2">Dribbble</li>
-          </ul>
+        <div className="lg:text-2xl text-xl underline">
+          <motion.ul variants={container} initial="hidden" animate="show">
+            <div className="overflow-hidden">
+              <motion.li variants={item} className="pb-2">
+                Twitter
+              </motion.li>
+            </div>
+            <div className="overflow-hidden">
+              <motion.li variants={item} className="pb-2">
+                Instagram
+              </motion.li>
+            </div>
+            <div className="overflow-hidden">
+              <motion.li variants={item} className="pb-2">
+                LinkedIn
+              </motion.li>
+            </div>
+            <div className="overflow-hidden">
+              <motion.li variants={item} className="pb-2">
+                Dribbble
+              </motion.li>
+            </div>
+          </motion.ul>
         </div>
       </div>
-    </main>
+    </motion.main>
   );
 }
